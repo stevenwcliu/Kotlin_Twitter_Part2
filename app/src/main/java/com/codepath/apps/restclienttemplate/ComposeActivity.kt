c27b313210fda85e376487e30b79a5c35efd8e2f
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-03-19 10:22:36 -0700

    updated

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
@@ -1,11 +1,15 @@
 package com.codepath.apps.restclienttemplate
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.codepath.apps.restclienttemplate.models.Tweet
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
@@ -15,8 +19,8 @@ class ComposeActivity : AppCompatActivity() {
 
     lateinit var etCompose: EditText
     lateinit var btnTweet: Button
-
     lateinit var  client: TwitterClient
+    lateinit var counter: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +30,33 @@ class ComposeActivity : AppCompatActivity() {
         btnTweet = findViewById(R.id.btnTweet)
 
         client = TwitterApplication.getRestClient(this)
+
+        counter = findViewById(R.id.counter)
+
+        etCompose.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                // Fires right as the text is being changed (even supplies the range of text)
+                if(etCompose.text.toString().trim().length < 141){
+                    counter.text = (140 - etCompose.text.toString().trim().length).toString()
+                    counter.setTextColor(Color.BLACK)
+                }
+                else{
+                    counter.text = "0"
+                    counter.setTextColor(Color.RED)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // Fires right before text is changing
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // Fires right after the text has changed
+
+            }
+        })
+
 
         // Handling the user's click on the tweet button
         btnTweet.setOnClickListener{
